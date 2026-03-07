4f7b70e5ed7decd7cee38b381e5c141c881e8834
Abhishek Rai <abhirai5515@gmail.com>
2026-03-07 21:11:54 +0530

    chore: fix vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
       '@/components': path.resolve(__dirname, './src/components'),
       '@/lib': path.resolve(__dirname, './src/lib'),
       '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/pages': path.resolve(__dirname, './src/pages')
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, '../shared')
     }
   }
-}) 
+})
